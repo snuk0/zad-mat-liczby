8a850b5cd675df7f831d96e3e55c6d1f27edf812
snuczek <26097711+snuk0@users.noreply.github.com>
2020-11-05 13:48:51 +0100

    Add files via upload

---
+++ 6.1/b/Zadanie05112020.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <bitset>
+#include <vector>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+vector <string> v;
+
+string oct2dec(string osemkowa)
+{
+	int dziesietna = strtol(osemkowa.c_str(), NULL, 8);
+	string s = to_string(dziesietna);
+
+	return s;
+}
+
+
+int main(int argc, char** argv) {
+	ifstream file1("liczby.txt");
+	ofstream fileOut;
+	if (file1.good())
+	{
+		string x;
+		int liczba;
+		while (file1 >> x) 
+		{
+			x = oct2dec(x);
+			if (x[0] == x[x.length() - 1])
+			{
+				v.push_back(x);
+			}
+			
+		}
+		fileOut.open("wyjscie.txt"); //otworz plik do wynikow
+		fileOut << v.size(); //zapisanie najmniejszej liczby z pliku czworkowego do pliku wynikowego
+		fileOut.close();
+
+	}
+}
